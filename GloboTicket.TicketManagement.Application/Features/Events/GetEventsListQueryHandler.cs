9ac4dd791ed142340fd614d2586d03a3a6a2cc4f
coders Club <codersclub.com>
2024-11-30 16:32:56 +0300

    commit

---
+++ GloboTicket.TicketManagement.Application/Features/Events/GetEventsListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListVm>>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+
+        public GetEventsListQueryHandler(IAsyncRepository<Event> eventRepository, IMapper mapper)
+        {
+            _eventRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+        {
+           var allEvents=(await _eventRepository.ListAllAsync()).OrderBy(x=>x.Date);
+            return _mapper.Map<List<EventListVm>>(allEvents);
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Profiles/MappingProfiels.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events;
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Profiles
+{
+   public class MappingProfiels:Profile
+    {
+        public MappingProfiels()
+        {
+            CreateMap<Event,EventListVm>().ReverseMap();
+            CreateMap<Event,EventDetailVm>().ReverseMap();
+            CreateMap<Category,CategoryDto>().ReverseMap();
+        }
+    }
+}
